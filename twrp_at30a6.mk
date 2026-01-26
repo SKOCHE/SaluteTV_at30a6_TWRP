438c896488813eba17876c1052e7fa2546d47e41
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2026-01-26 12:51:49 +0700

    Update twrp_at30a6.mk

--- twrp_at30a6.mk
+++ twrp_at30a6.mk
@@ -1,11 +1,14 @@
 # twrp_at30a6.mk for Android 13
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Определяем имя устройства для сборки
 PRODUCT_DEVICE := at30a6
 PRODUCT_NAME := twrp_at30a6
 PRODUCT_BRAND := SaluteTV
-PRODUCT_MODEL := at30a6 TWRP
+PRODUCT_MODEL := SaluteTV at30a6
 PRODUCT_MANUFACTURER := SaluteTV
+
+# Критически важный флаг для пропуска проверок зависимостей
+PRODUCT_BUILD_SUPPORT_ANY_OTA_IMAGE := true
+
