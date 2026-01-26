ef02d4a26e78c7119a04dc88436b35628550be23
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2026-01-26 12:47:08 +0700

    Update twrp_at30a6.mk

--- twrp_at30a6.mk
+++ twrp_at30a6.mk
@@ -1,24 +1,11 @@
 # twrp_at30a6.mk for Android 13
 
-# Inherit from TWRP
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/salute/at30a6/device.mk)
-
-# Device identifier
+# Определяем имя устройства для сборки
 PRODUCT_DEVICE := at30a6
 PRODUCT_NAME := twrp_at30a6
 PRODUCT_BRAND := SaluteTV
-PRODUCT_MODEL := SaluteTV
+PRODUCT_MODEL := at30a6 TWRP
 PRODUCT_MANUFACTURER := SaluteTV
-
-# TWRP Configuration
-PRODUCT_PACKAGES += \
-    twrpdecrypt
-
-# Set build fingerprint / ID
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="at30a6-user 13 TP1A.220905.004.A2 eng.jenkin.20251004.165826 release-keys"
-
-BUILD_FINGERPRINT := SaluteTV/at30a6/at30a6:13/TP1A.220905.004.A2/jenkins10041657:user/release-keys
